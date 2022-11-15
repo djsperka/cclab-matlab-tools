c11e3c5ba982e414436c6cd4e7d9862bfed03161
Daniel Sperka <djsperka@ucdavis.edu>
2022-11-15 09:55:46 -0800

    Stop usage of inputParser (speed). Args are which,widthMS,N,gapMS. Can supply 1-4 args.

--- cclabPulse.m
+++ cclabPulse.m
@@ -1,34 +1,60 @@
-function [success, s] = cclabPulse(varargin)
-%cclabPulse(which, msec=1, n=1, gap=1) 
-%   This is an async function, it may return before reward is done.
-    s = [];
+function [success, sample] = cclabPulse(varargin)
+%cclabPulse(which, widthMS=1, numPulses=1, gap=1) 
+%   Pulse 'which' channel ('A' or 'B'). Default is a single 1ms pulse. 
+    sample = [];
     success = 0;
     if ~cclabDIOIsReady()
         error('reward system not ready - call cclabInitReward()');
     else
         global g_dio;
-        p = inputParser;
-        addRequired(p, 'which', @(x) ischar(x) && length(x)==1 && (contains(x, 'A') || contains(x, 'B')));    % one at a time
-        addOptional(p, 'msec', 1, @(x) isnumeric(x) && isscalar(x) && (x > 0));
-        addOptional(p, 'n', 1, @(x) isnumeric(x) && isscalar(x) && (x > 0));
-        addOptional(p, 'gap', 1, @(x) isnumeric(x) && isscalar(x) && (x > 0));
-        parse(p, varargin{:});
+        nPulses = 1;
+        pulseGapMS = 1;
+        tPulseWidthMS = 1;
+        channel = '';
+        switch(nargin)
+        case 1
+            channel = varargin{1};
+        case 2
+            channel = varargin{1};
+            tPulseWidthMS = varargin{2};
+        case 3
+            channel = varargin{1};
+            tPulseWidthMS = varargin{2};
+            nPulses = varargin{3};
+        case 4
+            channel = varargin{1};
+            widthMS = varargin{2};
+            nPulses = varargin{3};
+            pulseGapMS = varargin{4};
+        otherwise
+            error('cclabPulse: InvalidNumberOfInputs', ...
+           'This function expects 0, 1, or 5 inputs.');
+        end
 
         % Which column, column 1 = 'A', column 2 = 'B'
         column = -1;
-        if contains(p.Results.which, 'A')
+        if contains(channel, 'A')
             column = 1;
-        elseif contains(p.Results.which, 'B')
+        elseif contains(channel, 'B')
             column = 2;
         end
 
-        % how many samples in the pulse?
-        % WARN: Assuming that daq set with 1K sampling rate
-        n = p.Results.n * p.Results.msec + (p.Results.n - 1) * p.Results.gap + 1;
+        % how many samples, at current rate, in a single pulse?
+        rate = g_dio.daqAB.Rate;
+        pSamples = floor(tPulseWidthMS / 1000 * rate);
+
+        % how many samples in the gaps?
+        gSamples = floor(pulseGapMS / 1000 * rate);
+
+        % total number of samples needed. 
+        % Note the number of channels is hard-coded here! Two columns.
+        % Only the channel 'A' or 'B' gets pulsed. 
+        n = nPulses*pSamples + (nPulses-1)*gSamples + 1;
         sample = zeros(n, 2);
-        for i=1:p.Results.n
-            sample((i-1)*(p.Results.msec + p.Results.gap)+1:(i-1)*(p.Results.msec + p.Results.gap)+p.Results.msec, column) = 1;
+        for i=1:nPulses
+            sample((i-1)*(pSamples + gSamples)+1:(i-1)*(pSamples + gSamples)+pSamples, column) = 1;
         end
         write(g_dio.daqAB, sample);
 
+    end
 end
