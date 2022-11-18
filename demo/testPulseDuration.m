24294020ec761b2d93262541068992c1a94f2f0f
unknown <cclab@cns-ddghj91m3.ou.ad3.ucdavis.edu>
2022-11-18 14:27:17 -0800

    Testing WIP

--- demo/testPulseDuration.m
+++ demo/testPulseDuration.m
@@ -2,7 +2,7 @@ function [times] = testPulseDuration(nPulses, tPulseLengthMS)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-
+Priority(2);
 times = zeros(nPulses, 1);
 t0 = GetSecs();
 cclabInitDIO('AB');
@@ -12,4 +12,5 @@ for i=1:nPulses
     %WaitSecs(.1);
 end
 cclabCloseDIO();
+Priority(0);
 return;
