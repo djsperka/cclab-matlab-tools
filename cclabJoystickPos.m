ef4cfa4a4abb1d97d1c8fe5941b759cbce0190ff
Daniel Sperka <djsperka@ucdavis.edu>
2024-01-16 13:22:36 -0800

    First attempt at correct dummy behavior.

--- cclabJoystickPos.m
+++ cclabJoystickPos.m
@@ -1,8 +1,7 @@
-function [status, joypos] = cclabJoystickPos()
+function joypos = cclabJoystickPos()
 %cclabJoyPos Get current joystick position. 
 %   Read joystick axes, return L=1, C=0, or R=-1
 
-status = -1;
 joypos = [];
 
 if ~cclabJoystickIsReady
@@ -11,26 +10,33 @@ end
 
 global g_dio
 
-A = read(g_dio.joystick.daq);
+if ~isempty(g_dio.joystick.daq)
+    A = read(g_dio.joystick.daq);
 
-% the timetable will have one column for each input channel
-status = 0;
-for icode = 1:length(g_dio.joystick.codes)
-    % S tells us if left is more positive than right (1 if so, -1 if not)
-    S = 1;
-    if g_dio.joystick.cal(icode,2) > g_dio.joystick.cal(icode,1)
-        S = -1;
+    % the timetable will have one column for each input channel
+    joypos = zeros(1, length(g_dio.joystick.codes));
+    for icode = 1:length(g_dio.joystick.codes)
+        % S tells us if left is more positive than right (1 if so, -1 if not)
+        S = 1;
+        if g_dio.joystick.cal(icode,2) > g_dio.joystick.cal(icode,1)
+            S = -1;
+        end
+    
+        vhi = max(g_dio.joystick.cal(icode,1), g_dio.joystick.cal(icode,2));
+        vlo = min(g_dio.joystick.cal(icode,1), g_dio.joystick.cal(icode,2));
+        if A{1, 1} >= vhi
+            pos = S;
+        elseif A{1, 1} >= vlo
+            pos = 0;
+        else
+            pos = -S;
+        end
+        joypos(icode) = pos;
     end
-
-    vhi = max(g_dio.joystick.cal(icode,1), g_dio.joystick.cal(icode,2));
-    vlo = min(g_dio.joystick.cal(icode,1), g_dio.joystick.cal(icode,2));
-    pos = nan;
-    if A{1, 1} >= vhi
-        pos = S;
-    elseif A{1, 1} >= vlo
-        pos = 0;
-    else
-        pos = -S;
+else
+    % random values
+    joypos = zeros(1, length(g_dio.joystick.codes));
+    for icode = 1:length(g_dio.joystick.codes)
+        joypos(icode) = randi([-1, 1]);
     end
-    joypos = horzcat(joypos, pos);
 end
