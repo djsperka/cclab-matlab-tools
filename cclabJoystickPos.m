00208aa2a8474170b4c56d77763015ecf0e89823
Daniel Sperka <djsperka@ucdavis.edu>
2024-03-07 10:43:03 -0800

    Fix index bug - was only using first channel even when two channels present. Change return to include raw voltages.

--- cclabJoystickPos.m
+++ cclabJoystickPos.m
@@ -1,8 +1,9 @@
-function joypos = cclabJoystickPos()
+function [joypos joyvolts] = cclabJoystickPos()
 %cclabJoyPos Get current joystick position. 
 %   Read joystick axes, return L=1, C=0, or R=-1
 
 joypos = [];
+joyvolts = [];
 
 if ~cclabJoystickIsReady
     error('Joystick not initialized.');
@@ -11,10 +12,13 @@ end
 global g_dio
 
 if ~isempty(g_dio.joystick.daq)
+
+    % read analog inputs. These are returned as a timetable, with a single
+    % row, one column per channel. 
     A = read(g_dio.joystick.daq);
 
-    % the timetable will have one column for each input channel
     joypos = zeros(1, length(g_dio.joystick.codes));
+    joyvolts = A{1, :};
     for icode = 1:length(g_dio.joystick.codes)
         % S tells us if left is more positive than right (1 if so, -1 if not)
         S = 1;
@@ -24,9 +28,9 @@ if ~isempty(g_dio.joystick.daq)
     
         vhi = max(g_dio.joystick.cal(icode,1), g_dio.joystick.cal(icode,2));
         vlo = min(g_dio.joystick.cal(icode,1), g_dio.joystick.cal(icode,2));
-        if A{1, 1} >= vhi
+        if A{1, icode} >= vhi
             pos = S;
-        elseif A{1, 1} >= vlo
+        elseif A{1, icode} >= vlo
             pos = 0;
         else
             pos = -S;
