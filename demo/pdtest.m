12a119f889c1a4a7376cbffa89e410c44e480a07
unknown <cclab@cns-dd1xsh0r3.ou.ad3.ucdavis.edu>
2024-07-10 16:46:46 -0700

    Update for tests using newinitdio

--- demo/pdtest.m
+++ demo/pdtest.m
@@ -1,18 +1,36 @@
-function [outputArg1,outputArg2] = pdtest(n, durationMS, gapMS)
+function pdtest(n, durationMS, screen_number)
 %pdtest flash lower right corner of screen n times.
 %   Detailed explanation goes here
 
 Screen('Preference', 'SkipSyncTests', 1);
-PsychImaging('PrepareConfiguration');
-PsychImaging('AddTask', 'General', 'FloatingPoint32Bit');
-PsychImaging('AddTask', 'FinalFormatting', 'DisplayColorCorrection', 'SimpleGamma');
+% PsychImaging('PrepareConfiguration');
+% PsychImaging('AddTask', 'General', 'FloatingPoint32Bit');
+% PsychImaging('AddTask', 'FinalFormatting', 'DisplayColorCorrection', 'SimpleGamma');
+% 
+%[wp, rect] = Screen('OpenWindow', 1, [127 127 127], [2160 1140 2560 1440]);
 
-[wp, rect] = PsychImaging('OpenWindow', 0, [127 127 127], [2160 1140 2560 1440]);
-
+cclabInitDIO('rig-left');
+[wp, ~] = Screen('OpenWindow', screen_number, [127 127 127]);
+WaitSecs(3);
+tstart = tic;
 for i=1:n
-    Screen('FillRect', wp, 255*mod(i,2)*[1 1 1]);
+    if mod(i,2)
+        Screen('FillRect', wp, 255*[1 1 1]);
+    else
+        %Screen('FillRect', wp, 127*[1 1 1]);
+        Screen('FillRect', wp, [0,0,0]);
+    end        
+    %Screen('FillRect', wp, 255*[1 1 1]);
     Screen('Flip', wp);
-    WaitSecs(durationMS/1000);
+    cclabPulse('A');
+    if (durationMS > 0)
+        WaitSecs(durationMS/1000);
+    end
 end
-
+t=toc(tstart);
 Screen('CloseAll');
+rate=n/t;
+
+disp(rate);
+
+cclabCloseDIO();
