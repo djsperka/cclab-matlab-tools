a9a61d59fead86b302da600ea67590f711cf5a15
Daniel Sperka <djsperka@ucdavis.edu>
2025-02-25 13:20:45 -0800

    Add argument for background color. Update to arguments block.

--- makeWindow.m
+++ makeWindow.m
@@ -1,6 +1,6 @@
-function [windowPtr,windowRect, mywr] = makeWindow(wh,screen,a1,a2)
+function [windowPtr,windowRect, mywr] = makeWindow(wh,screen,a1,a2,bkgd)
 %makeWindow(wh,screen,a1,a2) Make a window in the given screen,with given
-%size. Placement uses last two args - see AlignRect().
+%size. Placement uses 'a1' and 'a2' - see AlignRect().
 %   Convenience function for creating a PTB window. Placement of the
 %   window uses the values 'a1' and 'a2'. These are passed directly to
 %   AlignRect to determine where on the screen the window is placed. The
@@ -14,10 +14,20 @@ function [windowPtr,windowRect, mywr] = makeWindow(wh,screen,a1,a2)
 %   screen so it is screen 0), centered between top and bottom. The value
 %   of wrect is [0, 0, 400, 300]. The value of bigwrect is 
 %   [1520, 450, 1920, 750] (my screen size is 1920x1080). 
+%   The bkgd color should have values between 0-1, default is mean gray
+%   [0.5,0.5,0.5].
+
+    arguments
+        wh (1,2) {mustBeNumeric}
+        screen (1,1) {mustBeInteger,mustBeGreaterThanOrEqual(screen,0)}
+        a1 {mustBeText,mustBeMember(a1,{'center','left','right','top','bottom'})} = 'center'
+        a2 {mustBeText,mustBeMember(a2,{'center','left','right','top','bottom'})} = 'center'
+        bkgd (1,3) {mustBeNumeric,mustBeLessThanOrEqual(bkgd,1),mustBeGreaterThanOrEqual(bkgd,0)} = [.5, .5, .5]
+    end
     PsychDefaultSetup(2);
     r=SetRect(0,0,wh(1),wh(2));
     sr=Screen('Rect', screen);
     mywr=AlignRect(r,sr,a1,a2);
-    [windowPtr, windowRect] = PsychImaging('OpenWindow', screen, [.5 .5 .5], mywr);
+    [windowPtr, windowRect] = PsychImaging('OpenWindow', screen, bkgd, mywr);
 end
 
